83a112c5cf361e2ef9394b753d5465e30eda5151
Akrobate <fedorov.artiom@gmail.com>
2025-07-29 20:06:39 +0200

    Implementing businessState

--- code/NightLight/lib/BusinessState/BusinessState.h
+++ code/NightLight/lib/BusinessState/BusinessState.h
@@ -5,7 +5,17 @@
 class BusinessState {
 
     public:
+
         BusinessState();
+        String ssid = "";
+        String password = "";
+        String ap_name = "";
+        String ap_password = "";
+        
+        bool network_enabled = true;
+        int network_autodisable_delay_seconds= 100;
+
+        
 
 };
 
