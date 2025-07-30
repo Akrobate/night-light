f2c4853a547b429d4d44ee398a9ec4744dd85d37
Akrobate <fedorov.artiom@gmail.com>
2025-07-30 21:21:45 +0200

    Working on BusinessState.cpp

--- code/NightLight/lib/BusinessState/BusinessState.cpp
+++ code/NightLight/lib/BusinessState/BusinessState.cpp
@@ -1,5 +1,54 @@
 #include "BusinessState.h"
 
-BusinessState::BusinessState() {
-    
+// Constructeur
+BusinessState::BusinessState() {}
+
+// Getters
+String BusinessState::getSsid() const {
+    return ssid;
+}
+
+String BusinessState::getPassword() const {
+    return password;
+}
+
+String BusinessState::getApName() const {
+    return ap_name;
+}
+
+String BusinessState::getApPassword() const {
+    return ap_password;
+}
+
+bool BusinessState::isNetworkEnabled() const {
+    return network_enabled;
+}
+
+int BusinessState::getNetworkAutodisableDelaySeconds() const {
+    return network_autodisable_delay_seconds;
+}
+
+// Setters
+void BusinessState::setSsid(const String& s) {
+    ssid = s;
+}
+
+void BusinessState::setPassword(const String& p) {
+    password = p;
+}
+
+void BusinessState::setApName(const String& name) {
+    ap_name = name;
+}
+
+void BusinessState::setApPassword(const String& pass) {
+    ap_password = pass;
+}
+
+void BusinessState::setNetworkEnabled(bool enabled) {
+    network_enabled = enabled;
+}
+
+void BusinessState::setNetworkAutodisableDelaySeconds(int delay) {
+    network_autodisable_delay_seconds = delay;
 }
--- code/NightLight/lib/BusinessState/BusinessState.h
+++ code/NightLight/lib/BusinessState/BusinessState.h
@@ -13,9 +13,22 @@ class BusinessState {
         String ap_password = "";
         
         bool network_enabled = true;
-        int network_autodisable_delay_seconds= 100;
+        int network_autodisable_delay_seconds = 100;
 
-        
+        // Getters
+        String getSsid() const;
+        String getPassword() const;
+        String getApName() const;
+        String getApPassword() const;
+        bool isNetworkEnabled() const;
+        int getNetworkAutodisableDelaySeconds() const;
 
+        // Setters
+        void setSsid(const String& s);
+        void setPassword(const String& p);
+        void setApName(const String& name);
+        void setApPassword(const String& pass);
+        void setNetworkEnabled(bool enabled);
+        void setNetworkAutodisableDelaySeconds(int delay);
 };
 
