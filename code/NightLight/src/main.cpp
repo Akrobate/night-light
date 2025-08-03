9e4a3272cd0c6f702339e688ede0a3ea50462988
Akrobate <fedorov.artiom@gmail.com>
2025-08-04 00:59:09 +0200

    Woring on main

--- code/NightLight/src/main.cpp
+++ code/NightLight/src/main.cpp
@@ -1,7 +1,15 @@
 #include <Arduino.h>
 #include <LedInterface.h>
+#include <WifiService.h>
+#include <NightLightServer.h>
+#include <BusinessState.h>
 
 #include <configurations.h>
+
+LedInterface * led_interface = nullptr;
+WifiService * wifi_service = nullptr;
+NightLightServer * night_light_server = nullptr;
+BusinessState * business_state = nullptr;
 
 void setup() {
     Serial.begin(74880); // 115200
