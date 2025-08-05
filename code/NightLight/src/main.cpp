cdc38c44172588490ebc6a4ee5d14236f09a6ad0
Akrobate <fedorov.artiom@gmail.com>
2025-08-05 21:08:53 +0200

    work in progress

--- code/NightLight/src/main.cpp
+++ code/NightLight/src/main.cpp
@@ -12,7 +12,16 @@ NightLightServer * night_light_server = nullptr;
 BusinessState * business_state = nullptr;
 
 void setup() {
-    Serial.begin(74880); // 115200
+    Serial.begin(115200); // 74880
+
+    business_state = new BusinessState();
+    led_interface = new LedInterface();
+    wifi_service = new WifiService();
+    night_light_server = new NightLightServer();
+
+    night_light_server->injectBusinessState(business_state);
+    wifi_service->injectBusinessState(business_state);
+
 }
 
 
